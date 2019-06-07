cb5fe3c27cb7b26c033acbf1cbf56c9535c50956
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-07 11:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // Code your solutions in this file
-function countdown(input){
-while (-1<input) {
-  console.log(input--);
+function printBadges(badges) {
+  badges.forEach((name, i) => {
+    console.log(`Welcome ${name}! You are employee #${i + 1}.`)
+  })
+  return badges
 }
+
+function tailsNeverFails() {
+  tailsCount = 0
+
+  flip = Math.random()
+
+  while (flip >= 0.5) {
+    tailsCount ++
+    flip = Math.random()
+  }
+
+  return `You got ${tailsCount} tails in a row!`
 }
